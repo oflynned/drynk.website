358fdbebd97f97c74d4c9cf2fbb695e050fc2187
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-04 22:23:30 +0200

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <div>
+    <SEO title="Home" />
+    <div>drynk app</div>
+  </div>
+)
+
+export default IndexPage
